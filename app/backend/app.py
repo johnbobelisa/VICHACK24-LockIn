4c62d0ad07dbdbff44682f98b197990a8ea8fa5b
johnbobelisa <johnelisaaa@gmail.com>
2024-08-13 18:17:53 +1000

    commit initial flutter

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,46 +1,24 @@
-import os
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-VIDEO_DIRECTORY = 'static/videos/'
-ALLOWED_EXTENSIONS = ['mp4', 'webm', 'ogg']
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
+# Define a route for the root URL
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return 'Hello, World!'
 
-@app.route('/home')
-def home():
-    return render_template('index.html')
+# Define a route that returns JSON data
+@app.route('/data', methods=['GET'])
+def get_data():
+    data = {'message': 'This is data from the backend'}
+    return jsonify(data)
 
-
-@app.route('/upload/')
-def uploadclips():
-    return render_template('upload.html')
-
-@app.route('/clips/')
-def clips():
-    # List all video files in the directory
-    videos = [f for f in os.listdir(VIDEO_DIRECTORY) if allowed_file(f)]
-    return render_template('clips.html', videos=videos, video_dir=VIDEO_DIRECTORY)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'video' not in request.files:
-        return 'No video file found'
-    video = request.files['video']
-    if video.filename == "":
-        return 'No video file selected'
-    if video and allowed_file(video.filename):
-        video.save('static/videos/' + video.filename)
-        return render_template('clips.html', video_name=video.filename)
-    return 'invalid file type'
-
+# Define a route to receive data from the client
+@app.route('/post', methods=['POST'])
+def post_data():
+    content = request.json
+    print(content)  # Process the data as needed
+    return jsonify({'status': 'success', 'data': content})
 
 if __name__ == '__main__':
     app.run(debug=True)
